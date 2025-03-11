e60061e2ac855635047aa6be212aad790338201b
ElBewBew <benoit.vanhoe@gmail.com>
2025-03-11 15:33:34 +0100

    ajoute tout

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// filepath: c:\Users\ElBewBew\Desktop\Portfolio react\portfoliobv\tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
